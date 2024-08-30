ce157280f7366c85d2294de0caca6d64b9124e2c
Jack <1984742628@protonmail.com>
2024-08-31 01:01:08 +0200

    chore: change protocol version from 11 to 6

--- conn.go
+++ conn.go
@@ -19,7 +19,7 @@ import (
 const (
 	// protocolVersion is the current RakNet protocol version. This is Minecraft
 	// specific.
-	protocolVersion byte = 11
+	protocolVersion byte = 6
 
 	minMTUSize    = 576
 	maxMTUSize    = 1492
